e47f761b82698f0d6556f53b11b6de5164c62ee5
Phạm Minh Tâm <pminhtamnb@gmail.com>
2021-01-04 09:44:21 +0700

    add augument

--- data/data_provider.py
+++ data/data_provider.py
@@ -7,14 +7,16 @@ import glob
 import torchvision.transforms as transforms
 import numpy as np
 from data.data_transform import random_flip, random_rotate
-
+from data.dataset_utils import Augment_RGB_torch
+import random
 ##
 
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
     '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
 ]
-
+augment   = Augment_RGB_torch()
+transforms_aug = [method for method in dir(augment) if callable(getattr(augment, method)) if not method.startswith('_')]
 
 def random_cut(image_noise, image_gt, w, h=None):
     h = w if h is None else h
@@ -105,86 +107,15 @@ class SingleLoader(data.Dataset):
         image_noise = self.transforms(image_noise)
         image_gt = self.transforms(image_gt)
         image_noise, image_gt = random_cut(image_noise, image_gt, w=self.image_size)
+        apply_trans = transforms_aug[random.getrandbits(3)]
+
+        image_gt = getattr(augment, apply_trans)(image_gt)
+        image_noise = getattr(augment, apply_trans)(image_noise)
+
         return image_noise, image_gt
 
     def __len__(self):
         return len(self.noise_path)
 
 
-
-class MultiLoader(data.Dataset):
-    """
-    Args:
-
-     Attributes:
-        noise_path (list):(image path)
-    """
-
-    def __init__(self, noise_dir, gt_dir, image_size=512):
-
-        self.noise_dir = noise_dir
-        self.gt_dir = gt_dir
-        self.image_size = image_size
-        self.noise_path = glob.glob(self.noise_dir + "/*")
-        # for files_ext in IMG_EXTENSIONS:
-        #     self.noise_path.extend(glob.glob(self.noise_dir + "/*" + files_ext))
-        # self.gt_path = glob.glob(self.gt_dir + "/*")
-        # for files_ext in IMG_EXTENSIONS:
-        #     self.gt_path.extend(glob.glob(self.gt_dir + "/*" + files_ext))
-
-        if len(self.noise_path) == 0:
-            raise (RuntimeError("Found 0 images in subfolders of: " + self.noise_dir + "\n"
-                                                                                       "Supported image extensions are: " + ",".join(
-                IMG_EXTENSIONS)))
-
-        self.transforms = transforms.Compose([transforms.ToTensor()])
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, groundtrue) where image is a noisy version of groundtrue
-        """
-
-        path = self.noise_path[index]
-        list_path = sorted(glob.glob(path + "/*"))[:8]
-
-        name_folder_image = list_path[0].split("/")[-2].replace("NOISY_", "GT_")
-        name_image = list_path[0].split("/")[-1].replace("NOISY_", "GT_")
-        image_gt = Image.open(os.path.join(self.gt_dir, name_folder_image, name_image)).convert('RGB')
-        image_gt = self.transforms(image_gt)
-        ############
-        # Choose randcrop
-        w = self.image_size
-        h = self.image_size
-        nw = image_gt.size(-1) - w
-        nh = image_gt.size(-2) - h
-        if nw < 0 or nh < 0:
-            raise RuntimeError("Image is to small {} for the desired size {}". \
-                               format((image_gt.size(-1), image_gt.size(-2)), (w, h))
-                               )
-        # print("nw  : ",nw)
-        # idx_w = np.random.choice(nw + 1)
-        idx_w = torch.randint(0, nw + 1, (1,))[0]
-        # idx_h = np.random.choice(nh + 1)
-        idx_h = torch.randint(0, nh + 1, (1,))[0]
-
-        # idx_w = torch.randint(nw + 1)
-        # idx_h = torch.randint(nh+1)
-        # print(idx_w)
-        # print(idx_h)
-        ##########
-        image_gt_crop = image_gt[:, idx_h:(idx_h + h), idx_w:(idx_w + w)]
-
-        image_noise = [self.transforms(Image.open(img_path).convert('RGB'))[:, idx_h:(idx_h + h), idx_w:(idx_w + w)] for
-                       img_path in list_path]
-        image_noise_burst_crop = torch.stack(image_noise, dim=0)
-
-        # image_noise_burst_crop, image_gt_crop = random_cut_burst(image_noise_burst,image_gt,w = self.image_size)
-        return image_noise_burst_crop, image_gt_crop
-
-    def __len__(self):
-        return len(self.noise_path)
 
