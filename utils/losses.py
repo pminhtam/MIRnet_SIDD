ecf699dece7cd72723e3dce6e5dc55ac20ecd302
Phạm Minh Tâm <pminhtamnb@gmail.com>
2021-03-04 14:51:27 +0700

    fix loss

--- utils/losses.py
+++ utils/losses.py
@@ -147,6 +147,8 @@ class BasicLoss(nn.Module):
         self.beta = beta
 
     def forward(self, pred, burst_pred, gt, gamma):
+        b,N,c,h,w = burst_pred.size()
+        burst_pred = burst_pred.permute(b,c*N,h,w)
         burst_gt = torch.cat([gt[..., i::2, j::2] for i in range(2) for j in range(2)], dim=1)
 
         anneal_coeff = max(self.alpha ** gamma * self.beta, 1)
