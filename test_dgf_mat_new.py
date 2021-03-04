7847620b6c5d59f625ee2ac3f964ff4728f4277c
Phạm Minh Tâm <pminhtamnb@gmail.com>
2021-03-04 15:13:45 +0700

    add test new

---
+++ test_dgf_mat_new.py
@@ -0,0 +1,116 @@
+import torch
+import argparse
+from model.MIRNet import MIRNet_DGF
+
+from utils.metric import calculate_psnr,calculate_ssim
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.transforms as transforms
+from utils.training_util import load_checkpoint
+import math
+from PIL import Image
+import glob
+import time
+import scipy.io
+from data.data_provider import pixel_unshuffle
+
+# from torchsummary import summary
+
+def load_data_split(image_noise,burst_length):
+    image_noise_hr = image_noise
+    upscale_factor = int(math.sqrt(burst_length))
+    image_noise = pixel_unshuffle(image_noise, upscale_factor)
+    while len(image_noise) < burst_length:
+        image_noise = torch.cat((image_noise,image_noise[-2:-1]),dim=0)
+    if len(image_noise) > burst_length:
+        image_noise = image_noise[0:8]
+    image_noise_burst_crop = image_noise.unsqueeze(0)
+    return image_noise_burst_crop,image_noise_hr.unsqueeze(0)
+from data.dataset_utils import burst_image_filter
+def load_data_filter(image_noise,burst_length):
+    image_noise_hr = image_noise
+    image_noise = burst_image_filter(image_noise_hr)
+    image_noise_burst_crop = image_noise.unsqueeze(0)
+    return image_noise_burst_crop,image_noise_hr.unsqueeze(0)
+
+def test(args):
+    model = MIRNet_DGF()
+    # summary(model,[[3,128,128],[0]])
+    # exit()
+    if args.data_type == 'rgb':
+        load_data = load_data_split
+    elif args.data_type == 'filter':
+        load_data = load_data_filter
+    checkpoint_dir = args.checkpoint
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # try:
+    checkpoint = load_checkpoint(checkpoint_dir, device == 'cuda', 'latest')
+    start_epoch = checkpoint['epoch']
+    global_step = checkpoint['global_iter']
+    state_dict = checkpoint['state_dict']
+    model.load_state_dict(state_dict)
+    print('=> loaded checkpoint (epoch {}, global_step {})'.format(start_epoch, global_step))
+    # except:
+    #     print('=> no checkpoint file to be loaded.')    # model.load_state_dict(state_dict)
+    #     exit(1)
+    model.eval()
+    model = model.to(device)
+    trans = transforms.ToPILImage()
+    torch.manual_seed(0)
+    all_noisy_imgs = scipy.io.loadmat(args.noise_dir)['siddplus_valid_noisy_srgb']
+    all_clean_imgs = scipy.io.loadmat(args.gt)['siddplus_valid_gt_srgb']
+    # noisy_path = sorted(glob.glob(args.noise_dir+ "/*.png"))
+    # clean_path = [ i.replace("noisy","clean") for i in noisy_path]
+    i_imgs, _,_,_ = all_noisy_imgs.shape
+    psnrs = []
+    ssims = []
+    # print(noisy_path)
+    for i_img in range(i_imgs):
+        noise = transforms.ToTensor()(Image.fromarray(all_noisy_imgs[i_img]))
+        image_noise, image_noise_hr = load_data(noise, args.burst_length)
+        image_noise_hr = image_noise_hr.to(device)
+        burst_noise = image_noise.to(device)
+        begin = time.time()
+        _, pred = model(burst_noise,image_noise_hr)
+        pred = pred.detach().cpu()
+        gt = transforms.ToTensor()((Image.fromarray(all_clean_imgs[i_img])))
+        gt = gt.unsqueeze(0)
+        psnr_t = calculate_psnr(pred, gt)
+        ssim_t = calculate_ssim(pred, gt)
+        psnrs.append(psnr_t)
+        ssims.append(ssim_t)
+        print(i_img, "   UP   :  PSNR : ", str(psnr_t), " :  SSIM : ", str(ssim_t))
+        if args.save_img != '':
+            if not os.path.exists(args.save_img):
+                os.makedirs(args.save_img)
+            plt.figure(figsize=(15, 15))
+            plt.imshow(np.array(trans(pred[0])))
+            plt.title("denoise KPN DGF " + args.model_type, fontsize=25)
+            image_name = str(i_img) + "_"
+            plt.axis("off")
+            plt.suptitle(image_name + "   UP   :  PSNR : " + str(psnr_t) + " :  SSIM : " + str(ssim_t), fontsize=25)
+            plt.savefig(os.path.join(args.save_img, image_name + "_" + args.checkpoint + '.png'), pad_inches=0)
+    print("   AVG   :  PSNR : "+ str(np.mean(psnrs))+" :  SSIM : "+ str(np.mean(ssims)))
+
+
+if __name__ == "__main__":
+    # argparse
+    parser = argparse.ArgumentParser(description='parameters for training')
+    parser.add_argument('--noise_dir','-n', default='data/ValidationNoisyBlocksSrgb.mat', help='path to noise image file')
+    parser.add_argument('--gt','-g', default='data/ValidationGtBlocksSrgb.mat', help='path to noise image file')
+    # parser.add_argument('--noise_dir','-n', default='/home/dell/Downloads/noise/0001_NOISY_SRGB', help='path to noise image file')
+    parser.add_argument('--cuda', '-c', action='store_true', help='whether to train on the GPU')
+    parser.add_argument('--data_type', '-d', default="rgb", help='type of model : rgb, filter')
+    parser.add_argument('--burst_length', '-b', default=4, type=int, help='batch size')
+    parser.add_argument('--checkpoint', '-ckpt', type=str, default='checkpoint',
+                        help='the checkpoint to eval')
+    parser.add_argument('--image_size', '-sz', default=64, type=int, help='size of image')
+    parser.add_argument('--model_type','-m' ,default="DGF", help='type of model : DGF')
+    parser.add_argument('--save_img', "-s" ,default="", type=str, help='save image in eval_img folder ')
+
+    args = parser.parse_args()
+    #
+    # args.noise_dir = '/home/dell/Downloads/FullTest/noisy'
+    test(args)
+
