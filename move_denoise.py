6e4e0d3ad59a75872232df3ca49aa87b39abd1eb
MInh Tam <pminhtamnb@gmail.com>
2022-08-14 14:18:11 +0700

    final

--- move_denoise.py
+++ move_denoise.py
@@ -4,7 +4,7 @@ import os
 
 
 # img_paths = glob.glob("img_city/*.png")
-test_img = glob.glob("/vinai/tampm2/cityscapes_noise/leftImg8bit/val/*/*.png")
+test_img = glob.glob("/vinai/tampm2/cityscapes_noise/leftImg8bit_ori/val/*/*.png")
 
 # for img_path in img_paths:
 #     copy2(img_path,"../cityscape/gt/")
@@ -14,4 +14,4 @@ if not os.path.exists(save_path):
 
 for img in test_img:
     img_name = img.split("/")[-1]
-    copy2(os.path.join("img_city",img_name),os.path.dirname(img.replace("/leftImg8bit/","/leftImg8bit_denoise/")))
+    copy2(os.path.join("img_city10",img_name),os.path.dirname(img.replace("/leftImg8bit/","/leftImg8bit_denoise/")))
